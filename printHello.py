696ceaf18934ade5937a024a1ffbfdd202c8a880
rabihofcl <rabihofcl@gmail.com>
2021-09-13 17:28:04 +0530

    first commit

---
+++ printHello.py
@@ -0,0 +1,57 @@
+a = 'hghxkfbhghealiusoi;usmio;nosHvy9o5y8w73y95osunchdgbohivub7s89yn74wH9o8;dwlxm8nv73ye98vusy8ycinoHb pvnonuxeiysbwtc9f4nvrligbs87o b3tvolenybxd87ev8;oynkybv8o7dlby,hgxmdvnte7948oebhgvndrv 698ovkhnsvw9p38ouilhnkvje809l,gn04s8n9epu5ilkhnde9nf9voeikbez9fsp8okjdgx87o98ouh6rgvnxpgnnokjua90poqid2inkblq8ohudsjgmzegtawyiuq8oiapkslczdhureijowpqaskldjhm61578728738'
+
+for i in a:
+    if i== 'H' or i == 'h':
+        print(i , end="")
+        break
+
+for i in a:
+    if i== 'E' or i == 'e':
+        print(i , end="")
+        break
+
+for i in a:
+    if i== 'L' or i == 'l':
+        print(i, end="")
+        break
+
+for i in a:
+    if i== 'L' or i == 'l':
+        print(i , end="")
+        break
+
+for i in a:
+    if i== 'O' or i == 'o':
+        print(i , end="")
+        break
+
+for i in a:
+    if i== ' ' or i == ' ':
+        print(i , end="")
+        break
+
+for i in a:
+    if i== 'W' or i == 'w':
+        print(i , end="")
+        break
+
+for i in a:
+    if i== 'O' or i == 'o':
+        print(i , end="")
+        break
+
+for i in a:
+    if i== 'R' or i == 'r':
+        print(i , end="")
+        break
+
+for i in a:
+    if i== 'L' or i == 'l':
+        print(i , end="")
+        break
+
+for i in a:
+    if i== 'D' or i == 'd':
+        print(i , end="")
+        break
+
